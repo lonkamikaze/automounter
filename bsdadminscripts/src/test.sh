3c9a7cc5b31c3d53965a8f599547d4d33132f9ab
Dominic Fandrey <lonkamikaze@users.noreply.github.com>
2010-01-25 13:13:56 +0000

    A couple of fixes, mostly typos.

--- bsdadminscripts/src/test.sh
+++ bsdadminscripts/src/test.sh
@@ -1,10 +1,10 @@
 . bsda_pkg.sh
 bsda:pkg:Moved moved /var/db/uma/FTPMOVED
 bsda:pkg:Index index /var/db/uma/FTPINDEX $moved
-#$index.identifyPackages pkgs '*'
-#for pkgname in $(pkg_info -qoa | head -n 100); {
-#	$index.identifyPackages pkg "$pkgname"
-#}
+$index.identifyPackages pkgs '*'
+for pkgname in $(pkg_info -qoa | head -n 100); {
+	$index.identifyPackages pkg "$pkgname"
+}
 #echo half
 #for pkgname in $(pkg_info -qoa); {
 #	if ! $index.identifyPackages pkg "$pkgname"; then
